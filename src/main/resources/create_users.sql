57bbde9bb7fe1a216789231a9fe3c79670467b04
lklee <lklee@SCT13561.staffdmn.ouhk.edu.hk>
2019-03-22 17:25:09 +0800

    Added SQL file for creating user accounts.

---
+++ src/main/resources/create_users.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users (
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE user_roles (
+    user_role_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    username VARCHAR(50) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    PRIMARY KEY (user_role_id),
+    FOREIGN KEY (username) REFERENCES users(username)
+);
+
+INSERT INTO users VALUES ('keith', 'keithpw');
+INSERT INTO user_roles(username, role) VALUES ('keith', 'ROLE_USER');
+INSERT INTO user_roles(username, role) VALUES ('keith', 'ROLE_ADMIN');
+
+INSERT INTO users VALUES ('tsli', ' tslipw');
+INSERT INTO user_roles(username, role) VALUES ('tsli', 'ROLE_ADMIN');
+
+INSERT INTO users VALUES ('kevin', 'kevinpw');
+INSERT INTO user_roles(username, role) VALUES ('kevin', 'ROLE_USER');
+
+INSERT INTO users VALUES ('oliver', 'oliverpw');
+INSERT INTO user_roles(username, role) VALUES ('oliver', 'ROLE_USER');
