9f56adf2122b7a397773ab13afe867ca6e172437
lklee <lklee@SCT13561.staffdmn.ouhk.edu.hk>
2019-03-22 18:06:37 +0800

    Lab 09 Task 2 done.

--- src/main/resources/create_users.sql
+++ src/main/resources/create_users.sql
@@ -16,7 +16,7 @@ INSERT INTO users VALUES ('keith', 'keithpw');
 INSERT INTO user_roles(username, role) VALUES ('keith', 'ROLE_USER');
 INSERT INTO user_roles(username, role) VALUES ('keith', 'ROLE_ADMIN');
 
-INSERT INTO users VALUES ('tsli', ' tslipw');
+INSERT INTO users VALUES ('tsli', 'tslipw');
 INSERT INTO user_roles(username, role) VALUES ('tsli', 'ROLE_ADMIN');
 
 INSERT INTO users VALUES ('kevin', 'kevinpw');
