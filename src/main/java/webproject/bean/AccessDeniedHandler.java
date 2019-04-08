90ccf926a75d743916ec59fea9eceae4162a42e6
herbert <sap02ckh2@yahoo.com.hk>
2019-04-08 17:04:04 +0800

    Add url filters and redirect during 403

---
+++ src/main/java/webproject/bean/AccessDeniedHandler.java
@@ -0,0 +1,28 @@
+package webproject.bean;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandlerImpl;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Service
+public class AccessDeniedHandler extends AccessDeniedHandlerImpl {
+    private final String HOME_PAGE = "/";
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) throws IOException, ServletException {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && !(auth instanceof AnonymousAuthenticationToken)) {
+            response.sendRedirect(HOME_PAGE);
+        }
+
+        super.handle(request, response, e);
+    }
+}
